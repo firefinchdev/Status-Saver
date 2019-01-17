bb89e74429176828c897a49198fd078c32ea2e74
Anirudh Gupta <firefinchdev@gmail.com>
2019-01-18 00:02:44 +0530

    status fragment added

--- app/src/main/java/com/softinit/whatsdirect/fragments/StatusFragment.kt
+++ app/src/main/java/com/softinit/whatsdirect/fragments/StatusFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.softinit.whatsdirect.R
 
 class StatusFragment: androidx.fragment.app.Fragment() {
     companion object {
@@ -17,7 +18,18 @@ class StatusFragment: androidx.fragment.app.Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        val view: View =  inflater.inflate(R.layout.fragment_status, container, false)
+        setViewIds(view)
+        initiate(view)
+        return view
+    }
+
+    private fun setViewIds(view: View) {
+
+    }
+
+    private fun initiate(view: View) {
+
     }
 
 }
