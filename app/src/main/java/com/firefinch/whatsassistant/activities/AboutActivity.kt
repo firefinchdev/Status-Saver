178f4b5b7bfdb771a39c6ba4c5f1399dc0d2a0ea
Anirudh Gupta <firefinchdev@gmail.com>
2019-02-16 19:08:27 +0530

    add: privacy policy

--- app/src/main/java/com/firefinch/whatsassistant/activities/AboutActivity.kt
+++ app/src/main/java/com/firefinch/whatsassistant/activities/AboutActivity.kt
@@ -5,22 +5,31 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import com.firefinch.whatsassistant.R
+import com.firefinch.whatsassistant.utils.AppIntent
 import com.firefinch.whatsassistant.utils.getOpenSourceDialog
 
 class AboutActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnOSLicense: Button
+    private lateinit var btnPrivacyPolicy: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
         btnOSLicense = findViewById(R.id.btn_licenses)
-        btnOSLicense.setOnClickListener(this)
+        btnPrivacyPolicy = findViewById(R.id.btn_privacy_policy)
+
+        listOf(btnOSLicense,btnPrivacyPolicy).forEach { it.setOnClickListener(this)}
     }
     override fun onClick(v: View?) {
         when(v?.id) {
             R.id.btn_licenses -> {
                 getOpenSourceDialog(this).show()
+            }
+            R.id.btn_privacy_policy -> {
+                startActivity(AppIntent
+                    .viewInBrowser(getString(R.string.privacy_policy_url))
+                )
             }
         }
     }
